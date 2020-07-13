4b43e2fccb5165801f57d74f110dfb6e347757d3
PD Jell <mystral7@naver.com>
2020-07-13 17:34:05 +0900

    init commit.

---
+++ Poster/Poster/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Poster
+//
+//  Created by YooHG on 7/13/20.
+//  Copyright © 2020 Jell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var gogo: String = "" {
+        willSet(after) {
+            print("willset: \(after)")
+        }
+        didSet(before) {
+            print("didset: \(before)")
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        gogo = "1"
+        gogo = "2"
+    }
+
+    @IBAction func buttonClicked(_ sender: Any) {
+        let vc: UIAlertController = UIAlertController.init(title: "경고", message: "눌렀구나", preferredStyle: .alert)
+        vc.addAction(.init(title: "가즈아", style: .default, handler: { action in
+            print(action.title!)
+        }))
+        present(vc, animated: true, completion: nil)
+    }
+    
+}
+
